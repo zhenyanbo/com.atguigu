562e0e9eabdc0058bae6b97a2a5ea80dc5e9c8e1
yanbo <2621042651@qq.com>
2023-12-01 12:02:42 +0800

    hot-fix demo

--- src/main/java/com/atguigu/git/test.java
+++ src/main/java/com/atguigu/git/test.java
@@ -4,5 +4,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hello,world");
         System.out.println("test111111111");
+        System.out.println("aaaaaaaaaaa");
+        System.out.println("bbbbbbbbbbbbbbbbbbb");
     }
 }
