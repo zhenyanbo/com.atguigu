238b3d85101c5671a8c60a6ee44a04a178a4cca0
yanbo <2621042651@qq.com>
2023-12-01 14:16:47 +0800

    push demo

--- src/main/java/com/atguigu/git/test.java
+++ src/main/java/com/atguigu/git/test.java
@@ -8,5 +8,6 @@ public class test {
         System.out.println("bbbbbbbbbbbbbbbbbbb");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push 成功");
     }
 }
